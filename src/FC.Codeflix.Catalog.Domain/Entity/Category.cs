57b1aa249438bcf1876865207be226f0002b04f8
giovani.franz <giovani.franz@agi.com.br>
2023-03-25 17:41:21 -0300

    feat/add validate category

--- src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -1,17 +1,9 @@
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
 namespace FC.Codeflix.Catalog.Domain.Entity;
 
 public class Category
 {
-    public Guid Id { get; private set; }
-    
-    public bool IsActive { get; private set; }
-    
-    public DateTime CreatedAt { get; private set; }
-    
-    public string Name { get; private set; }
-    
-    public string Description { get; private set; }
-    
     public Category(string name, string description, bool isActive = true)
     {
         Id = Guid.NewGuid();
@@ -19,5 +11,25 @@ public class Category
         Description = description;
         IsActive = isActive;
         CreatedAt = DateTime.Now;
+        Validate();
+    }
+
+    public Guid Id { get; }
+
+    public bool IsActive { get; }
+
+    public DateTime CreatedAt { get; }
+
+    public string Name { get; }
+
+    public string Description { get; }
+
+    private void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+
+        if (Description == null)
+            throw new EntityValidationException($"{nameof(Description)} should not be empty or null");
     }
 }
--- tests/FC.Codeflix.Catalog.UnitTests/Entity/Category/CategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Entity/Category/CategoryTest.cs
@@ -1,5 +1,7 @@
+using FC.Codeflix.Catalog.Domain.Exceptions;
 using Xunit;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
 namespace FC.Codeflix.Catalog.UnitTests.Entity.Category;
 
 public class CategoryTest
@@ -8,51 +10,69 @@ public class CategoryTest
     [Trait("Domain", "Category - Aggregates")]
     public void Instantiate()
     {
-
         var validData = new
         {
             Name = "Categoy name",
-            Description = "Category description",
-            
+            Description = "Category description"
         };
         var datetimeBefore = DateTime.Now;
         var category = new DomainEntity.Category(validData.Name, validData.Description);
         var datetimeAfter = DateTime.Now;
-        
+
         Assert.NotNull(category);
         Assert.Equal(validData.Name, category.Name);
         Assert.Equal(validData.Description, category.Description);
-        Assert.NotEqual(default(Guid), category.Id);
-        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.NotEqual(default, category.Id);
+        Assert.NotEqual(default, category.CreatedAt);
         Assert.True(category.CreatedAt > datetimeBefore);
         Assert.True(category.CreatedAt < datetimeAfter);
         Assert.True(category.IsActive);
     }
-    
+
     [Theory(DisplayName = nameof(InstantiateWithIsActive))]
     [Trait("Domain", "Category - Aggregates")]
     [InlineData(true)]
     [InlineData(false)]
     public void InstantiateWithIsActive(bool isActive)
     {
-
         var validData = new
         {
             Name = "Categoy name",
-            Description = "Category description",
-            
+            Description = "Category description"
         };
+
         var datetimeBefore = DateTime.Now;
         var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
         var datetimeAfter = DateTime.Now;
-        
+
         Assert.NotNull(category);
         Assert.Equal(validData.Name, category.Name);
         Assert.Equal(validData.Description, category.Description);
-        Assert.NotEqual(default(Guid), category.Id);
-        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.NotEqual(default, category.Id);
+        Assert.NotEqual(default, category.CreatedAt);
         Assert.True(category.CreatedAt > datetimeBefore);
         Assert.True(category.CreatedAt < datetimeAfter);
         Assert.Equal(isActive, category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsEmpty))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("     ")]
+    public void InstantiateErrorWhenNameIsEmpty(string? name)
+    {
+        Action action = () => new DomainEntity.Category(name!, "Description");
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Name should not be empty or null", exception.Message);
+    }
+
+    [Fact(DisplayName = nameof(InstantiateErrorWhenDescriptionIsNull))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void InstantiateErrorWhenDescriptionIsNull()
+    {
+        Action action = () => new DomainEntity.Category("Name", null!);
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Description should not be empty or null", exception.Message);
     }
 }
